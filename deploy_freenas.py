a297bf6a1cbaf23e987734ec868c3429274a38ee
Ian G <imgrant@users.noreply.github.com>
2021-10-13 09:58:38 +0100

    Add support for changing S3 service certificate

--- deploy_freenas.py
+++ deploy_freenas.py
@@ -53,6 +53,7 @@ PRIVATEKEY_PATH = deploy.get('privkey_path',"/root/.acme.sh/" + DOMAIN_NAME + "/
 FULLCHAIN_PATH = deploy.get('fullchain_path',"/root/.acme.sh/" + DOMAIN_NAME + "/fullchain.cer")
 PROTOCOL = deploy.get('protocol','http://')
 PORT = deploy.get('port','80')
+S3_ENABLED = deploy.getboolean('s3_enabled',fallback=False)
 FTP_ENABLED = deploy.getboolean('ftp_enabled',fallback=False)
 WEBDAV_ENABLED = deploy.getboolean('webdav_enabled',fallback=False)
 CERT_BASE_NAME = deploy.get('cert_base_name','letsencrypt')
@@ -151,6 +152,23 @@ else:
   print ("Error setting active certificate!")
   print (r.text)
   sys.exit(1)
+
+if S3_ENABLED:
+  # Set our cert as active for S3 plugin
+  r = session.put(
+    BASE_URL + '/api/v2.0/s3/',
+    verify=VERIFY,
+    data=json.dumps({
+      "certificate": cert_id,
+    }),
+  )
+
+  if r.status_code == 200:
+    print ("Setting active S3 certificate successful")
+  else:
+    print ("Error setting active S3 certificate!")
+    print (r)
+    sys.exit(1)
 
 if FTP_ENABLED:
   # Set our cert as active for FTP plugin
